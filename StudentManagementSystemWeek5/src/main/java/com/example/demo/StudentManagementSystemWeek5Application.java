c39e56fc21e74756a468686d548a055c587bdf06
Manjunath K <manjmanju532@gmail.com>
2025-07-06 20:53:33 +0530

    Add files via upload

---
+++ StudentManagementSystemWeek5/src/main/java/com/example/demo/StudentManagementSystemWeek5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemWeek5Application.class, args);
+	}
+
+}
